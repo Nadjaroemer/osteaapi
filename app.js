02355b0c445e4c95a25e860764209b83d7d8a2eb
Nadjaroemer <nadja.kuti@gmail.com>
2020-12-14 20:46:31 +0100

    initital commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var express = require("express");
+var app = express();
+var formidable = require("express-formidable");
+
+require("./database");
+app.use(formidable());
+
+//sereveren bliver startet
+require("./cheeses.route")(app);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("App is running on", process.env.PORT || 3000);
+})
