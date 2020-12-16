400c57d3826618f415feab48e37c3b5d9d742031
Nadjaroemer <nadja.kuti@gmail.com>
2020-12-16 20:10:20 +0100

    added cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 require("./database");
+app.use(cors());
 app.use(formidable());
 
 //sereveren bliver startet
